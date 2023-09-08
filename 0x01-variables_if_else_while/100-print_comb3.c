7448e5758dd3e62610b948d42b3318005712d6e4
Pheogrammer <ngelime07@gmail.com>
2023-09-08 10:06:30 -0700

    combo3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d1, d2;
+
+	for (d1 = 0; d1 <= 8; d1++)
+	{
+		for (d2 = d1 + 1; d2 <= 9; d2++)
+		{
+			putchar((d1 % 10) + '0');
+			putchar((d2 % 10) + '0');
+
+			if (d1 == 8 && d2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
