edd7d54149136c03e34a603e66c7869485bdabc2
Pheogrammer <ngelime07@gmail.com>
2023-09-08 09:32:24 -0700

    numbersz in lines

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+	}
+	putchar('\n');
+	return (0);
+}
