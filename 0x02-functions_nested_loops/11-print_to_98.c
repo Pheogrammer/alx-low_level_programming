8c1e327a7b1170442a402c8a525dfa1847a224eb
Pheogrammer <ngelime07@gmail.com>
2023-09-12 09:19:27 -0700

    refactored 11 positive numbers;

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,41 +8,54 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		while (n <= 98)
+		for (; n <= 98; n++)
 		{
 			if (n < 0)
 			{
 				_putchar('-');
 				n *= -1;
 			}
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
+			print_positive_numbers(n);
 			if (n != 98)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			n++;
 		}
 	}
 	else
 	{
-		while (n >= 98)
+		for (; n >= 98; n--)
 		{
 			if (n < 0)
 			{
 				_putchar('-');
 				n *= -1;
 			}
-			_putchar((n / 10) + '0');
-			_putchar((n % 10) + '0');
+			print_positive_numbers(n);
 			if (n != 98)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			n--;
 		}
 	}
 	_putchar('\n');
+}
+
+/**
+ * print_positive_numbers - prints all natural numbers from n to 98
+ * @n: the starting number
+ */
+void print_positive_numbers(int n)
+{
+	if (n >= 10)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else
+	{
+		_putchar(n + '0');
+	}
 }
