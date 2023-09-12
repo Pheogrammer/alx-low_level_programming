b122e36e562a1448ef885adb4e9dc485c241e06e
Pheogrammer <ngelime07@gmail.com>
2023-09-12 08:30:50 -0700

    times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,25 +12,21 @@ void times_table(void)
 		for (column = 0; column <= 9; column++)
 		{
 			result = row * column;
-			if (column > 0)
+			if (column == 0)
+			{
+				_putchar(result + '0');
+			}
+			else
 			{
 				_putchar(',');
 				_putchar(' ');
 				if (result < 10)
 					_putchar(' ');
 				else
-					_putchar('\n');
-			}
-			if (result >= 10)
-			{
-				_putchar(result / 10 + '0');
+					_putchar(result / 10 + '0');
 				_putchar(result % 10 + '0');
 			}
-			else
-			{
-				_putchar(result + '0');
-			}
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
