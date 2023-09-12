c5890bd63b59615b5d4972b0ba9e7a814fd05710
Pheogrammer <ngelime07@gmail.com>
2023-09-12 10:25:00 -0700

    fixes

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,13 +23,13 @@ void print_times_table(int n)
 			else
 			{
 				_putchar(',');
-				_putchar(' ');
 				if (result < 10)
 					_putchar(' ');
 				if (result < 100)
 					_putchar(' ');
 				if (result >= 100)
 				{
+					_putchar(' ');
 					_putchar(result / 100 + '0');
 					_putchar((result / 10) % 10 + '0');
 				}
