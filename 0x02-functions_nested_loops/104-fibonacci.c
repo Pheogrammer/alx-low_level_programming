035092f380426ce19e89b2dc2bc47b7a2b537dfa
Pheogrammer <ngelime07@gmail.com>
2023-09-12 13:50:57 -0700

    swfs

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,65 +1,78 @@
 #include <stdio.h>
 
+#define MAX_DIGITS 200
 
-void addLargeNumbers(int num1[], int num2[], int result[], int size) {
-    int carry, i, sum;
-   carry = 0;
-    for (i = size - 1; i >= 0; i--) {
+void initialize_number(unsigned int num[]) {
+    int i;
+	for (i = 0; i < MAX_DIGITS; i++) {
+        num[i] = 0;
+    }
+}
+
+void add_numbers(unsigned int result[], unsigned int num1[], unsigned int num2[]) {
+    int carry = 0;
+int i, sum;
+    for (i = 0; i < MAX_DIGITS; i++) {
         sum = num1[i] + num2[i] + carry;
         result[i] = sum % 10;
         carry = sum / 10;
     }
 }
 
-
-void printLargeNumber(int num[], int size) {
-    int leadingZero;
+void copy_number(unsigned int dest[], unsigned int src[]) {
     int i;
-
-   leadingZero = 1;
-    for (i = 0; i < size; i++) {
-        if (num[i] != 0) {
-            leadingZero = 0;
-        }
-        if (!leadingZero) {
-            putchar('0' + num[i]);
-        }
-    }
-    if (leadingZero) {
-        putchar('0'); 
+	for (i = 0; i < MAX_DIGITS; i++) {
+        dest[i] = src[i];
     }
 }
 
-int main() {
-	int i;
-	int j;
-int fib1[200] = {0};
-int fib2[200] = {0};
-int sum[200] = {0};
+void print_number(unsigned int num[]) {
+    int leading_zero, i;
+   leading_zero = 1;
 
-    fib1[199] = 1; 
-    fib2[199] = 1; 
+    for (i = MAX_DIGITS - 1; i >= 0; i--) {
+        if (num[i] != 0) {
+            leading_zero = 0;
+        }
 
-    printLargeNumber(fib1, 200);
-    putchar(','); 
-    putchar(' ');
-    printLargeNumber(fib2, 200);
-
-    for (i = 2; i < 98; i++) {
-        addLargeNumbers(fib1, fib2, sum, 200);
-        putchar(',');
-        putchar(' ');
-        printLargeNumber(sum, 200);
-
-        
-        for (j = 0; j < 200; j++) {
-            fib1[j] = fib2[j];
-            fib2[j] = sum[j];
+        if (!leading_zero) {
+            putchar(num[i] + '0');
         }
     }
 
-    putchar('\n');
+    if (leading_zero) {
+        putchar('0');
+    }
+}
 
+void print_fibonacci(int count) {
+    unsigned int fib1[MAX_DIGITS];
+    unsigned int fib2[MAX_DIGITS];
+    unsigned int temp[MAX_DIGITS];
+    int i;
+
+    initialize_number(fib1);
+    initialize_number(fib2);
+    fib1[MAX_DIGITS - 1] = 1;
+    fib2[MAX_DIGITS - 1] = 1;
+
+    print_number(fib1);
+
+    for (i = 2; i <= count; i++) {
+        add_numbers(temp, fib1, fib2);
+        copy_number(fib1, fib2);
+        copy_number(fib2, temp);
+
+        putchar(',');
+        putchar(' ');
+        print_number(fib1);
+    }
+
+    putchar('\n');
+}
+
+int main(void) {
+    print_fibonacci(100);
     return 0;
 }
 
