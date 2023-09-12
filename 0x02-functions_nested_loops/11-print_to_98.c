e3176412e0d4316202a6d01acd9084fe81401704
Pheogrammer <ngelime07@gmail.com>
2023-09-12 09:57:10 -0700

    betty vs 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,61 +1,40 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * print_positive_numbers - prints all natural numbers from n to 98
- * @n: the starting number
- */
-void print_positive_numbers(int n)
-{
-	if (n >= 10)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else
-	{
-		_putchar(n + '0');
-	}
-}
-
-/**
- * print_to_98 - prints all natural numbers from n to 98
- * @n: the starting number
- */
+* print_to_98 - prints all natural numbers from @n to 98
+* @n: number to start from
+*
+* Return: Always void
+*/
 void print_to_98(int n)
 {
+	int step;
+	int digit;
+
 	if (n <= 98)
 	{
-		for (; n <= 98; n++)
-		{
-			if (n < 0)
-			{
-				_putchar('-');
-				n *= -1;
-			}
-			print_positive_numbers(n);
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
+		step = 1;
 	}
 	else
 	{
-		for (; n >= 98; n--)
-		{
-			if (n < 0)
-			{
-				_putchar('-');
-				n *= -1;
-			}
-			print_positive_numbers(n);
-			if (n != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
+		step = -1;
 	}
-	_putchar('\n');
+
+	while (n != 98)
+	{
+		if (n < 0)
+		{
+			putchar('-');
+			n *= -1;
+		}
+		digit = n / 10;
+		putchar(digit + '0');
+		digit = n % 10;
+		putchar(digit + '0');
+		putchar(', ');
+		n += step;
+	}
+	putchar('9');
+	putchar('8');
+	putchar('\n');
 }
