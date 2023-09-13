59e127dd2b9188921c678600fb794a3cfc9e3d68
Pheogrammer <ngelime07@gmail.com>
2023-09-13 11:16:24 -0700

    mahn

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,60 @@
-#include "main.h"
-/** * print_to_98 - Prints all natural numbers from n to 98. 
- * * @n: The starting number. */
+#include "holberton.h"
+
+/**
+ * print_to_98 - Prints all natural numbers from n to 98.
+ * @n: The starting number.
+ */
 void print_to_98(int n)
 {
-	long i;
-	
-	if (n <= 98)
-	{
-		for (i = n; i <= 98; i++) {            if (i != n)                _putchar(',');            _putchar(' ');            if (i < 10)                _putchar(' ');            if (i < 100)                _putchar(' ');            if (i < 0)            {                _putchar('-');                i = -i;            }            print_number((int)i);        }    }    else    {        for (i = n; i >= 98; i--)        {            if (i != n)                _putchar(',');            _putchar(' ');            if (i < 10)                _putchar(' ');            if (i < 100)                _putchar(' ');            if (i < 0)            {                _putchar('-');                i = -i;            }            print_number((int)i);        }    }    _putchar('\n');}
+    long i;
+
+    if (n <= 98)
+    {
+        for (i = n; i <= 98; i++)
+        {
+            if (i != n)
+                _putchar(',');
+            _putchar(' ');
+
+            if (i < 10)
+                _putchar(' ');
+
+            if (i < 100)
+                _putchar(' ');
+
+            if (i < 0)
+            {
+                _putchar('-');
+                i = -i;
+            }
+
+            print_number((int)i);
+        }
+    }
+    else
+    {
+        for (i = n; i >= 98; i--)
+        {
+            if (i != n)
+                _putchar(',');
+            _putchar(' ');
+
+            if (i < 10)
+                _putchar(' ');
+
+            if (i < 100)
+                _putchar(' ');
+
+            if (i < 0)
+            {
+                _putchar('-');
+                i = -i;
+            }
+
+            print_number((int)i);
+        }
+    }
+
+    _putchar('\n');
+}
 
