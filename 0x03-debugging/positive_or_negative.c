80344282a18fc58ae16d395d2956733286bd6b31
Pheogrammer <ngelime07@gmail.com>
2023-09-13 10:20:39 -0700

    debugger

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,16 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int positive_or_negative(int n)
 {
-	int n;
-
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("%d is ", n);
