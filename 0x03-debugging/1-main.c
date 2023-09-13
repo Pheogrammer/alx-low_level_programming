22f94687c24c47581311598231a3b0531372293b
Pheogrammer <ngelime07@gmail.com>
2023-09-13 10:50:47 -0700

    looper

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,11 +10,15 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
+
 	i = 0;
-	/**while (i < 10)*/
+
+	/*while (i < 10)*/
 	/*{*/
-	/*putchar(i);*/
+	/*	putchar(i);*/
 	/*}*/
+
 	printf("Infinite loop avoided! \\o/\n");
+
 	return (0);
 }
