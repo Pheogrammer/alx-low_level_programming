af33bedb9d0400daea2c4d6d7becaabd750cd34a
Pheogrammer <ngelime07@gmail.com>
2023-09-14 13:52:33 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * mul - checks for digit character.
+ *
+ * @c: character to check.
+ *
+ * @b: character to check.
+ *
+ * Return: Always 0.
+ */
+int mul(int c, int b)
+{
+	int x;
+
+	x = mul(c, b);
+	return (x);
+}
