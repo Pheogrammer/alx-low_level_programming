28302fe08753ac4268818de4bcd691b27f4a03fa
Pheogrammer <ngelime07@gmail.com>
2023-09-14 14:06:15 -0700

    many files

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,7 @@
-#include <ctype.h>
-#include <stdio.h>
-
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
-int mul(int a, int b)
+int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -11,5 +9,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-void print_number(int n);;
-
+void print_number(int n);
