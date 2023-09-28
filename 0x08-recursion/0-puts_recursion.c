ca458469f31c2c86506e7a1de045b29bed959ceb
Pheogrammer <ngelime07@gmail.com>
2023-09-28 13:50:49 -0700

    1,2

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_puts_recursion - prints a string followed by a newline
+ *
+ *@s:string
+ *
+ *Return:void
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+	_putchar(*s);
+	_puts_recursion(s + 1);
+	}
+}
