e974796cd74215e0f3a4f35c3d4482a8a12226e3
Alfeo Raymond <ngelime07@gmail.com>
2023-10-08 07:57:58 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,48 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-/**
- * multiply - multiply two numbers.
- * @num1: fisrt parameter.
- * @num2: second parameter.
- * Return: mutipled number.
- */
-int multiply(int num1, int num2)
-{
-	return (num1 * num2);
+
+int is_digit(char c) {
+  return c >= '0' && c <= '9';
 }
-/**
- * main - multiplies two positive numbers.
- * @argc: fisrt parameter.
- * @argv: second parameter.
- * Return: always 0.
- */
-int main(int argc, char *argv[])
-{
-	int num1, num2, result;
-	int i, j;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		return (98);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (98);
-			}
-		}
-	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+int multiply(int num1, int num2) {
+  int result = 0;
+  int carry = 0;
+  while (num2 > 0) {
+    int product = (num1 * (num2 % 10) + carry) % 10;
+    carry = (num1 * (num2 % 10) + carry) / 10;
+    result += product;
+    num2 /= 10;
+  }
+  result += carry;
+  return result;
+}
 
-	result = multiply(num1, num2);
-	printf("%d\n", result);
+int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    printf("Error\n");
+    exit(98);
+  }
 
-	return (0);
+  for (int i = 0; i < strlen(argv[1]); i++) {
+    if (!is_digit(argv[1][i])) {
+      printf("Error\n");
+      exit(98);
+    }
+  }
+
+  for (int i = 0; i < strlen(argv[2]); i++) {
+    if (!is_digit(argv[2][i])) {
+      printf("Error\n");
+      exit(98);
+    }
+  }
+
+  int num1 = atoi(argv[1]);
+  int num2 = atoi(argv[2]);
+
+  if (num1 < 0 || num2 < 0) {
+    printf("Error\n");
+    exit(98);
+  }
+
+  printf("%d\n", multiply(num1, num2));
+
+  return 0;
 }
