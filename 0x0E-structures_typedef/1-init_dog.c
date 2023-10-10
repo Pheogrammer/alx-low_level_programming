b12a41af9c1d6588bce0486c43e3f3a6f39397dd
Alfeo Raymond <ngelime07@gmail.com>
2023-10-10 19:37:23 +0300

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 /**
  *init_dog -initializes a variable of type dog
  *
- *@d:struct
+ *@dog:struct
  *@name:name of dog
  *@age:age of dog
  *@owner:name of owner
@@ -10,10 +10,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != '\0')
-	{
-		(*d).name = name;
-		(*d).age = age;
-		(*d).owner = owner;
-	}
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 }
