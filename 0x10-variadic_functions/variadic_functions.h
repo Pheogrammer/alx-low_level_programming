f9dd92e57e9486484898772ad2ef6fd0e6f00a32
Pheogrammer <ngelime07@gmail.com>
2023-10-13 03:27:18 -0700

    j

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,3 +1,5 @@
+#ifndef _VARIADIC_FUNCTIONS_H_
+#define _VARIADIC_FUNCTIONS_H_
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
@@ -5,3 +7,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+#endif
