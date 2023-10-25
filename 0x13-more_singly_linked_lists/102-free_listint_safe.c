63b5d9ee74259aeca644dddee09395f7c6e468f0
Pheogrammer <ngelime07@gmail.com>
2023-10-24 18:33:48 -0700

    102

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <string.h>
+#include <stddef.h>
 
 size_t looped_listint_count(listint_t *head);
 size_t free_listint_safe(listint_t **h);
@@ -13,42 +15,42 @@ size_t free_listint_safe(listint_t **h);
  */
 size_t looped_listint_count(listint_t *head)
 {
-	listint_t *tortoise, *hare;
-	size_t nodes = 1;
+listint_t *tortoise, *hare;
+size_t nodes = 1;
 
-	if (head == NULL || head->next == NULL)
-		return (0);
+if (head == NULL || head->next == NULL)
+return (0);
 
-	tortoise = head->next;
-	hare = (head->next)->next;
+tortoise = head->next;
+hare = (head->next)->next;
 
-	while (hare)
-	{
-		if (tortoise == hare)
-		{
-			tortoise = head;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-				hare = hare->next;
-			}
+while (hare)
+{
+if (tortoise == hare)
+{
+tortoise = head;
+while (tortoise != hare)
+{
+nodes++;
+tortoise = tortoise->next;
+hare = hare->next;
+}
 
-			tortoise = tortoise->next;
-			while (tortoise != hare)
-			{
-				nodes++;
-				tortoise = tortoise->next;
-			}
+tortoise = tortoise->next;
+while (tortoise != hare)
+{
+nodes++;
+tortoise = tortoise->next;
+}
 
-			return (nodes);
-		}
+return (nodes);
+}
 
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
-	}
+tortoise = tortoise->next;
+hare = (hare->next)->next;
+}
 
-	return (0);
+return (0);
 }
 
 /**
@@ -63,34 +65,34 @@ size_t looped_listint_count(listint_t *head)
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *tmp;
-	size_t nodes, index;
+listint_t *tmp;
+size_t nodes, index;
 
-	nodes = looped_listint_count(*h);
+nodes = looped_listint_count(*h);
 
-	if (nodes == 0)
-	{
-		for (; h != NULL && *h != NULL; nodes++)
-		{
-			tmp = (*h)->next;
-			free(*h);
-			*h = tmp;
-		}
-	}
+if (nodes == 0)
+{
+for (; h != NULL && *h != NULL; nodes++)
+{
+tmp = (*h)->next;
+free(*h);
+*h = tmp;
+}
+}
 
-	else
-	{
-		for (index = 0; index < nodes; index++)
-		{
-			tmp = (*h)->next;
-			free(*h);
-			*h = tmp;
-		}
+else
+{
+for (index = 0; index < nodes; index++)
+{
+tmp = (*h)->next;
+free(*h);
+*h = tmp;
+}
 
-		*h = NULL;
-	}
+*h = NULL;
+}
 
-	h = NULL;
+h = NULL;
 
-	return (nodes);
+return (nodes);
 }
