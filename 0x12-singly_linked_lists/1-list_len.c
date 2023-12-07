c1a3b6b3cb2fedb2af498e0f656d1c5fdffaa8e3
root <pheogrammer@gmail.com>
2023-12-06 23:18:16 -0800

    0x12

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+  *list_len - give the number of elements in a list.
+  *@h: pointer to list.
+  *
+  *Return: number of elements in a linked list.
+  */
+size_t list_len(const list_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+
+	return (i);
+}
